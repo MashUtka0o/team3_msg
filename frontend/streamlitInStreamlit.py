2b3e90b3c3a23d0256889c3fb91f87d0ae498d3a
chris <christopher.reinard@gmail.com>
2024-06-21 23:21:48 +0200

    initial commit

---
+++ frontend/streamlitInStreamlit.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+curr_question_list = []
+
+
+def add_question():
+    return
+
+
+def render_questions_list(question_list):
+    return
+
+
+def save_list():
+    return
+
+
+st.button("Add Question", on_click=add_question)
+st.button("Save List", on_click=save_list)
+
+# Form Component Looks like this:
