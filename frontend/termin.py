38537827b19b7c0e42d85e92e177f971ef42ebda
christopher-reinard <christopher.reinard@gmail.com>
2024-06-22 11:20:15 +0200

    Create termin.py
    made termin.py thingy

---
+++ frontend/termin.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import sqlite3
+
+# Date, Location, Doctor name, Summary, Documents?
+
+ex_termin = ("2024-06-25", "Max Mustermann", "Karlsruhe", None, "20489234", "abc@email.com")
+
+
+def open_termin(termin):
+    st.header("Termininformationen")
+    date = st.write("Date: " + termin[0])
+    info = st.write(termin[1] + ", " + termin[2])
+    summary = termin[3]
+    if summary is None:
+        st.write(":red[Form not Filled, Please Fill Form]")
+    # This Button Opens a small tab
+
+    with st.expander("Important Information"):
+        st.write("Please bring with you all of your money in your bank account")
+        if summary is None:
+            st.button("Fill in Form NOW", on_click=open_questionnaire)
+
+    c1, c2, c3 = st.columns([1, 1, 2])
+    with c1:
+        st.button("Cancel Appointment")
+    with c2:
+        st.button("Reschedule Appointment")
+
+
+def open_questionnaire():
+    navigate_to("fragebogen.py")
+
+
+# location = s
+# docName
+# patName
+# sum
+
+
+open_termin(ex_termin)
